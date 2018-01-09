70c5bfc294b0ae5f87ddfc07a97a540a9a1c826b
Melissa Bell <melissabell456@gmail.com>
2018-01-09 08:48:19 -0600

    set up directories and installed grunt

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+      jshint: {
+          files: ['javascripts/**/*.js'],
+          options: {
+              predef: ['document', 'console', '$', 'alert'],
+              esnext: true,
+              globalstrict: true,
+              globals: {},
+              browserify: true
+          }
+      },
+      sass: {
+          dist: {
+              files: {
+                  'stylesheets/main.css': 'sass/style.scss'
+              }
+          }
+      },
+      watch: {
+          javascripts: {
+              files: ['javascripts/**/*.js'],
+              tasks: ['jshint', 'browserify']
+          },
+          sass: {
+              files: ['sass/**/*.scss'],
+              tasks: ['sass']
+          }
+      },
+      browserify: {
+          'dist/bundle.js': ['javascripts/main.js']
+      }
+  });
+  require('matchdep').filter('grunt-*').forEach(grunt.loadNpmTasks);
+  grunt.registerTask('default', ['jshint', 'sass', 'browserify', 'watch']);
+}
